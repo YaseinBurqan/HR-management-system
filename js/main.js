5cfe85700914546a41f91a6685b923ebfb73dee5
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-04-20 16:12:35 +0300

    HR-management-system

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function Employees(ID, Name, Department, Level, Image, Salary) {
+  this.ID = 0;
+  this.name = "name";
+  this.Department = 0;
+  this.Level = 0;
+  this.Image = 0;
+  this.Salary = 0;
+}
